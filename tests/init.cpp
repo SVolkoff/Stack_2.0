015cd6a0f2bff03d6cb78da2f975b07a3255e1ad
SVolkoff <sabina.volkoff@gmail.com>
2017-10-16 10:55:11 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,59 +1,42 @@
-#include <stack.hpp>
 #include <catch.hpp>
+#include <stack.hpp>
 
-SCENARIO(" init")
+SCENARIO("stack init", "[init]")
 {
-	stack<int> s1();
-	REQUIRE(s1.count() == 0);
-}
-SCENARIO("init with object")
-{ 
-	stack<int> s1(); 
-	s1.push(5); 
-	s1.push(6); 
-  	stack<int> s2(s1());
-	REQUIRE(s1.count() == s2.count());
-	REQUIRE(s2.pop() == 6);
-	REQUIRE(s1.pop() == 5);
+	stack<int> st;
+	REQUIRE(st.count() == 0);
 }
 
-SCENARIO("push") 
+SCENARIO("stack init object", "[init object]") 
 {
-	stack<int> s1();
-	s1.push(1);
-	REQUIRE(s1.count() == 1);
-	s1.push(2);
-	REQUIRE(s1.count() == 2);
-	s1.push(3);
-	REQUIRE(s1.count() == 3);
+	stack<int> st1;
+
+	st1.push(1);
+	st1.push(2);
+	st1.push(3);
+	stack<int> st2(st1);
+	REQUIRE(st1.count() == st2.count());
 }
 
-SCENARIO("pop") 
+SCENARIO("stack op=", "[op=]")
 {
-	stack<int> s1();
-	s1.push(1);
-	s1.push(2);
-	s1.push(3);
-	int val = s1.pop();
-	REQUIRE(s1.count() == 2 && val == 3);
-	val = s1.pop();
-	REQUIRE(s1.count() == 1 && val == 2);
-	val = s1.pop();
-	REQUIRE(s1.count() == 0 && val == 1);
+	stack<int> st1;
+
+	st1.push(1);
+	st1.push(2);
+	st1.push(3);
+	stack<int> st2 = st1;
+	REQUIRE(st1.count() == st2.count() == 3);
 }
 
-SCENARIO("Stack operator= with swap") {
-	stack<int> s1();
-	stack<int> s2();
-	s1.push(1);
-	s1.push(2);
-	s1.push(3);
-	s2.push(4);
-	REQUIRE(s1.count() == 3);
-	s2 = s1;
-	REQUIRE(s2.count() == 3);
-	REQUIRE(s1.pop() == s2.pop());
-	REQUIRE(s1.pop() == s2.pop());
-	REQUIRE(s1.pop() == s2.pop());
-	
+SCENARIO("stack push", "[push]")
+{
+	stack<int> st;
+	REQUIRE(st.count() == 0);
+	st.push(6);
+	REQUIRE(st.count() == 1);
+	st.push(7);
+	REQUIRE(st.count() == 2);
+	st.push(78);
+	REQUIRE(st.count() == 3);
 }
