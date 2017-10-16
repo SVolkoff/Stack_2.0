1e3d4dcddd9edde103fd1cce1f0b2ca6b9186366
SVolkoff <sabina.volkoff@gmail.com>
2017-10-16 13:36:25 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -45,6 +45,7 @@ stack<T>::stack(const stack<T>& other)
 {
 	array_size_ = other.array_size_;
 	count_ = other.count_;
+	array_=new T[count_];
 	std::copy(other.array_, other.array_ + other.count_, array_);
 }
 
